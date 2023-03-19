4658fc4ad5561babaeed41cd94186135c295392d
JP-Kigochi <mbotejohnpaul@gmail.com>
2023-03-19 15:59:41 -0700

    Add 6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  *main - 'printf string to terminal'
  *
  *Description: 'prints the set of words to terminal'
@@ -13,6 +13,6 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
