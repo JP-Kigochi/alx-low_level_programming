21ba263a94c768581ef7ac0b6d4529d73c73f91a
JP-Kigochi <mbotejohnpaul@gmail.com>
2023-03-19 16:49:13 -0700

    Fixed bugs in 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  *
  *@parameterr void
  *
- *Return: Always successful 0
+ *Return: Failure 1
  *
  */
 
@@ -16,5 +16,5 @@ int main(void)
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(STDERR_FILENO, str, 59);
-	return (0);
+	return (1);
 }
