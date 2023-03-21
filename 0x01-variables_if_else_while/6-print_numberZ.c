0900bdc2754c44d5564a324cdf4619c9265f9ead
JP-Kigochi <mbotejohnpaul@gmail.com>
2023-03-21 05:29:21 -0700

    Add 6-print_numberZ.c

---
+++ 0x01-variables_if_else_while/6-print_numberZ.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - returns 0 if code block is executed succesfully
+ *
+ *Description: Numbers 0 to 9 are printed to screen using putchar
+ *
+ *@parameter void
+ *
+ *Return: Always successful 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+	int n;
+
+	for (i=0, n =48; i < 10; i++)
+	{
+		putchar(i + n);
+	}
+
+	putchar('\n');
+}
