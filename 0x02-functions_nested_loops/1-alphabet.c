a2982d1f3ce8772f6bc3b32089d037f322de8d16
JP-Kigochi <mbotejohnpaul@gmail.com>
2023-03-28 01:57:09 -0700

    Fixed bugs in 1-alphabet.c file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,12 +11,12 @@
 
 void print_alphabet(void)
 {
-	char alphabet[26] = 'abcdefghijklmnopqrstuvwxyz';
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0; i < 26; i++)
 	{
-		_putchar('%c', alphabet[i]);
+		_putchar(alphabet[i]);
 	}
 	_putchar('\n');
 }
