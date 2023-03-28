f5348246fcb51b82ed97e6cc90d88fc72b2c885a
JP-Kigochi <mbotejohnpaul@gmail.com>
2023-03-28 04:36:43 -0700

    Fixed bugs in 7-print-last-digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,10 @@
 int print_last_digit(int n)
 {
 	int lastDigit;
-	
-	
+
+
 	lastDigit = n % 10;
-	if (n < 10)
+	if (n < 0)
 		lastDigit = -lastDigit;
 	_putchar(lastDigit + '0');
 	return (lastDigit);
