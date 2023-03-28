76ddf2427b5ab9187b8578e8fd2c260be061e6a5
JP-Kigochi <mbotejohnpaul@gmail.com>
2023-03-28 03:27:34 -0700

    Add 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_abs - prints absolute value
+ *
+ *@int: value to be made absolute
+ *
+ *Return: int when positive and the absolute int when negative
+ *
+ */
+
+int _abs(int)
+{
+	if (int >= 0)
+		return (int);
+	else
+		return (0 - int);
+}
