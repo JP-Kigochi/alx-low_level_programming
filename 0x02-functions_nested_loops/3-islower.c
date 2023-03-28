a203b15a53d82a6424bc375f4420fc884e0a6ca9
JP-Kigochi <mbotejohnpaul@gmail.com>
2023-03-28 02:36:33 -0700

    Fixed logical bug in 3-islower.c file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,14 +5,14 @@
  *
  *@c: number to be checked
  *
- *Return: 0 when lowercase and 1 when not
+ *Return: 1 when lowercase and 0 when not
  *
  */
 
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-		return (0);
-	else
 		return (1);
+	else
+		return (0);
 }
