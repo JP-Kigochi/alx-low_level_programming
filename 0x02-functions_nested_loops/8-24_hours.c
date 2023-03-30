cbc393b729f0beca5cfad959cd1f045322daf8f9
JP-Kigochi <mbotejohnpaul@gmail.com>
2023-03-30 05:47:12 -0700

    Added 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *jack_bauer - prints out hours and minutes of the day
+ *
+ *@param void
+ *
+ *Return: void(nothing)
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar('0' + hour / 10);
+			_putchar('0' + hour % 10);
+			_putchar(':');
+			_putchar('0' + minute / 10);
+			_putchar('0' + minute % 10);
+			_putchar('\n');
+		}
+	}
+}
