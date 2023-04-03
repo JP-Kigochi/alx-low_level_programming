475d35e5a7bd2a4489cf6221ffa3250db0e6a9a2
JP-Kigochi <mbotejohnpaul@gmail.com>
2023-04-03 00:12:48 -0700

    Added 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ *print_to_98 - prints all natural numbers from n to 98
+ *
+ *@n: starting integer in sequence
+ *
+ *Return: void
+ *
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			_putchar('0' + i);
+			_putchar(',');
+			_putchar(" ");
+		}
+	}
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			_putchar('0' + i);
+			_putchar(',');
+			_putchar(" ");
+		}
+	}
+
+	_putchar('0' + 9);
+	_putchar('0' + 8);
+	_putchar('\n');
+}
