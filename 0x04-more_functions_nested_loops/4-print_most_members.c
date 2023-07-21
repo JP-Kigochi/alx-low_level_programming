86d3aa2b96d97a75a1df07d6b5dd095b0dc63638
JP-Kigochi <mbotejohnpaul@gmail.com>
2023-07-21 14:23:20 +0300

    Add 4-print_most_members.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_members.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - print 0-9 skipping
+ *2 and 4
+ *@param void
+ *Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
