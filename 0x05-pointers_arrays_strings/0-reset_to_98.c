cd694d88562b187153699e3998c4cb3322b3d922
JP-Kigochi <mbotejohnpaul@gmail.com>
2023-07-24 21:52:52 +0300

    Add 0-reset_to_98.c file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - updates value to 98
+ *@param void
+ *@*n: pointer to the address of number
+ *to be changed to 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
