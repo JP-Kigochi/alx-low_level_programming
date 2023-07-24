da28b0f920d3eeb7a03f2681909fa8d0a34c24eb
JP-Kigochi <mbotejohnpaul@gmail.com>
2023-07-24 22:47:45 +0300

    Add 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_puts - prints a string to standard output
+ *@str: The string argument
+ *Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
