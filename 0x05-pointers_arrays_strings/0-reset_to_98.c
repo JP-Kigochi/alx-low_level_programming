e79e4b0ec457c9e75bd29e20a37199bdeaa2de0f
JP-Kigochi <mbotejohnpaul@gmail.com>
2023-07-24 22:14:07 +0300

    Fixed betty style issues in 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 
 /**
  *reset_to_98 - updates value to 98
- *@param void
- *@*n: pointer to the address of number
+ *@n: pointer to the address of number
  *to be changed to 98
+ *Return: void
  */
 void reset_to_98(int *n)
 {
