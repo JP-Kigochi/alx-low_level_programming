49b05281c405d2bb413a033b89be84602ae6d9bd
JP-Kigochi <mbotejohnpaul@gmail.com>
2023-07-25 22:27:54 +0300

    Changed do..while to while loop

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,13 @@ void print_rev(char *s)
 	int counter;
 
 	counter = 0;
-	do {
+	while (*s != '\0')
+	{
 		s++;
 		counter++;
-	} while (*s != '\0');
-	s--;
+	}
 
-	while (counter > 0)
+	while (counter >= 0)
 	{
 		_putchar(*s);
 		s--;
