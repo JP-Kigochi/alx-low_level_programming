df86e338dfc35db85f7d789ca0e5a613d6e6b8c7
JP-Kigochi <mbotejohnpaul@gmail.com>
2023-07-25 09:48:17 +0300

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_rev - prints string in rverse order
+ *@s: characters to be printed
+ *Return: void
+ */
+void print_rev(char *s)
+{
+	int counter;
+
+	counter=0;
+	do
+	{
+		s++;
+		counter++;
+	} while (*s != '\0');
+
+	while (counter > 0)
+	{
+		_putchar(*s);
+		s--;
+		counter--;
+	}
+	_putchar('\n');
+}
